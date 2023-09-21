68897d2c21f6812ac8f897aee879131edc7e5700
tahmid-emam <0.1tahmidemam@gmail.com>
2023-09-21 17:36:06 +0600

    some css are added

--- src/Components/Men/SingleProduct.jsx
+++ src/Components/Men/SingleProduct.jsx
@@ -4,8 +4,10 @@ const SingleProduct = (props) => {
     const {id, title, image} = props.product;
   return (
     <>
-      {/* <h1>{title}</h1> */}
+      <div>
+      <h1>{title}</h1>
       <img src={image} alt="" />
+      </div>
     </>
   )
 }
